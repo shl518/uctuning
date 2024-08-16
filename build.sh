020551a33d249c7e6692401262c664bac72ee1d2
shizy <shizy@act.buaa.edu.cn>
2024-08-16 10:51:46 +0800

    0816

---
+++ build.sh
@@ -0,0 +1,7 @@
+mc alias set shahe http://10.212.253.24:9000 shizy i7FVURSmrifOaDaD98RMyCloQu8qKOtnLFPSEuwF
+mkdir llama3-7b
+mkdir data
+mc cp -r  shahe/shizy/llama3-7b/ ./llama3-7b
+mc cp shahe/uc-tuning/TruthfulQA.csv ./data
+pip install accelerate
+pip install transformers
